0217d8d06dfcce53eae1a28470dd4b6814bb6289
monsantoshi <monsan@gmail.com>
2024-01-30 10:13:24 +0700

    cc

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pdfkit
 import time
 
 # Set your OpenAI Assistant ID here
-assistant_id = 'asst_Enter your assistant ID here'
+assistant_id = 'asst_2rqx8jV1PVz6MQxp0kLZ9GT2'
 
 # Initialize the OpenAI client (ensure to set your API key in the sidebar within the app)
 client = openai
